edadf9dc449a54d3c94e11a94745cbb74dabeef8
Shai Feldman <shai.feldman@mail.huji.ac.il>
2024-11-03 15:22:26 +0200

    lab1

---
+++ lab1/lab1.py
@@ -0,0 +1,8 @@
+#############################################################
+# FILE : lab1.py
+# WRITER : Shai Feldman , shai.feldman , 332519636
+# EXERCISE : intro2cs lab1 2025
+# DESCRIPTION: A simple program that prints "Hello World!" to
+# the standard output (screen).
+#############################################################
+print("Hello World!")
