8735cf604ec47a11ccea3fd865d25f60bd3cc1cd
Shai Feldman <shai.feldman@mail.huji.ac.il>
2024-11-06 11:13:53 +0200

    Add comments

--- excercises/ex1/hello_turtle.py
+++ excercises/ex1/hello_turtle.py
@@ -1,24 +1,25 @@
-import turtle
 import turtle as tr
 
 
 def draw_triangle():
     """This function draws a triangle"""
-    tr.forward(45)
-    tr.right(120)
-    tr.forward(45)
-    tr.right(120)
-    tr.forward(45)
-    tr.right(120)
+    for i in range(3):
+        tr.forward(45)
+        tr.right(120)
 
 
 def draw_sail():
     """This function draws a sail"""
+    # draw middle line
     tr.left(90)
     tr.forward(50)
+
+    # draw the triangle
     tr.right(150)
     draw_triangle()
     tr.right(30)
+
+    # return turtle to original place
     tr.up()
     tr.forward(50)
     tr.down()
@@ -27,11 +28,16 @@ def draw_sail():
 
 def draw_ship():
     """This function draws a ship"""
+    # draw the upper part
     tr.right(90)
     tr.forward(50)
     for i in range(3):
         draw_sail()
         tr.forward(50)
+
+    # draw the floor
+    tr.up()
+    tr.left(90)
     tr.right(120)
     tr.forward(20)
     tr.right(60)
@@ -67,4 +73,4 @@ def draw_fleet():
 
 if __name__ == '__main__':
     draw_fleet()
-    turtle.done()
+    tr.done()
