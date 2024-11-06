02e1edd2de530d1e7cbcb983f23a92ba356a4640
Shai Feldman <shai.feldman@mail.huji.ac.il>
2024-11-06 11:33:37 +0200

    Fix intro example

--- excercises/ex1/intro_example.py
+++ excercises/ex1/intro_example.py
@@ -1,8 +1,7 @@
-import turtle
-
 def intro_example():
- """This is only an example method for printing hello"""
- print ("hello")
+    """This is only an example method for printing hello"""
+    print("hello")
+
 
 if __name__ == '__main__':
     intro_example()
